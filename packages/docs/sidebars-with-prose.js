fa34aa9dba2ce519ac540b6286dc9b2a0515acae
Petyo Ivanov <underlog@gmail.com>
2020-09-08 10:31:55 +0300

    docs: split packages into separate sections

--- packages/docs/sidebars-with-prose.js
+++ packages/docs/sidebars-with-prose.js
@@ -3,7 +3,12 @@ const typedoc = require("./sidebars").typedoc;
 module.exports = {
   typedoc: {
     Introduction: ["get-started", "urx-by-example", "thinking-in-systems"],
-    "API Reference": typedoc.Modules,
+    "@virtuoso.dev/urx": typedoc.Modules.filter((id) =>
+      /^modules\/_urx/.test(id)
+    ),
+    "@virtuoso.dev/react-urx": typedoc.Modules.filter((id) =>
+      /^modules\/_react_urx/.test(id)
+    ),
     Interfaces: typedoc.Interfaces,
   },
 };
